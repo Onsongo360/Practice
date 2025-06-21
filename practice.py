0d0b7ec95879d04e769193bb02ba03354ecae513
Jeremy Onsongo <jerryonsongo@gmail.com>
2025-06-21 17:27:20 +0300

    List comprehension

---
+++ practice.py
@@ -0,0 +1,10 @@
+animals = ['lion', 'tiger', 'monkey', 'elephant', 'frog']
+filtered_animals = []
+for animal in animals:
+    filtered_animals.append(animal.title())
+filtered_animals
+
+# Using list comprehension
+
+filtered_animals = animal.title() for animal in animals
+filtered_animals
